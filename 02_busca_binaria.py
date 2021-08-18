7c6f068be75ba5be8b3cf6f7da0aba5998c27aa5
LEANDRO0486 <leandrohenrique1804@gmail.com>
2021-08-18 17:48:28 +0000

    (13/08) busca sequencial e busca binaria

--- 02_busca_binaria.py
+++ 02_busca_binaria.py
@@ -6,3 +6,55 @@
 # feitas até que se encontre o valor de busca ou que reste apenas uma
 # sublista vazia, quando se conclui que o valor de busca não existe na lista.
 
+from time import time
+from data.lista_nomes import nomes 
+
+comps = 0
+
+def busca_binaria(lista, valor_busca):
+    """
+       função que implementa o algoritimo de busca biária
+
+       Retorna a posição onde valor_busca foi encontrado ou
+       ou valor convencial -1 se valor_busca não existir na lista
+    """
+    ini = 0                   # primeira posição
+    fim = len(lista) - 1      # útima posição
+
+    while ini <= fim:
+        meio = (ini + fim) // 2     # Operador //(duas barras) é divisão inteira
+
+        # 1º caso: lista[meio] é igual a valor busca
+        if lista[meio] == valor_busca:  # Encontrou!
+            return meio  # meio é a posição onde valor_busca está na lista
+        
+        # 2º caso: valor_busca é menor que lista[meio]
+        elif valor_busca <lista[meio]:
+            fim = meio - 1  # descarta a 2º metade da lista
+
+        # 3º caso: valor_busca é maior que lista[meio]
+        else:
+            ini = meio + 1   #descartar a 1º metade da lista
+
+        # 4º caso: valor_busca não encontrado na lista
+    return - 1
+
+hora_ini = time()
+print(f"posição de LEANDRO: {busca_binaria(nomes, 'LEANDRO')}")
+hora_fim = time()
+print(f"Tempo gasto procurando LEANDRO: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"posição de ZULEICA: {busca_binaria(nomes, 'ZULEICA')}")
+hora_fim = time()
+print(f"Tempo gasto procurando ZULEICA: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"posição de ZUZIVALDO: {busca_binaria(nomes, 'ZUZIVALDO')}")
+hora_fim = time()
+print(f"Tempo gasto procurando ZUZIVALDO: {(hora_fim - hora_ini) * 1000}ms")
+
+hora_ini = time()
+print(f"posição de BELERINA: {busca_binaria(nomes, 'BELERINA')}")
+hora_fim = time()
+print(f"Tempo gasto procurando BELERINA: {(hora_fim - hora_ini) * 1000}ms")
