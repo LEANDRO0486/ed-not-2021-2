3d5a3842d3b8239fbe6f700124146b92613dbf69
LEANDRO0486 <leandrohenrique1804@gmail.com>
2021-08-19 23:30:25 +0000

    (19/08) Término do bubble sort e início do selection sort

---
+++ 04_selection_sort.py
@@ -0,0 +1,8 @@
+# ALGORITIMO DE ORDENAÇÃO SELECTION SORT
+#
+# Isola (seleciona) o primeiro elemento da lista e, em seguida, 
+# encontra o menor valor no restante da lista. Se o valor encontrado 
+# for menor que o valor selecionado, efetua a troca.
+# Em seguida, isola o segundo número da lista, buscando pelo menor 
+# valor das posições subsequentes. Faz a troca entre as os dois valores,
+# se necessario. Continua o processo, até isolar o penultimo da lista.
